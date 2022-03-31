ed83d6fcb4ccbd7b83d3e4502d983ce4dad3d13d
Prince Trambadiya <princetrambadiya1212@gmail.com>
2022-03-30 23:25:42 -0400

    EDIT: requirements.txt

--- main.py
+++ main.py
@@ -126,4 +126,4 @@ def resultCSV():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0",port=int(os.environ.get('PORT',8080)))
+    app.run(host='127.0.0.1', port=8080, debug=True)
